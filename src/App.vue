db8587c016121476d21234d705515d403fd5e639
pablopsdigital <pablopsdigital@gmail.com>
2020-05-22 19:20:26 +0200

    create initial components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <px-header></px-header>
+    <px-assets-table></px-assets-table>
+  </main>
+</template>
+
+<script>
+//Importar componentes
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
+
+export default {
+  name: "App",
+  components: {
+    PxHeader,
+    PxAssetsTable
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
